4085a0c67063e71366b5054b9cf8c4d8de01c8c5
Atila Xavier <atila.xavier@gmail.com>
2020-05-04 23:18:07 -0300

    Adjusted for very long number of files/folders on root folder

--- CoversBr_download.py
+++ CoversBr_download.py
@@ -27,7 +27,7 @@ import base64
 import argparse
 
 
-def Dl_file(fchild, local_folder_name):
+def Dl_file(s, fchild, local_folder_name):
 	fname = fchild['name']
 	fsize = fchild['size']
 	dl_url = fchild['@content.downloadUrl']
@@ -42,7 +42,7 @@ def Dl_file(fchild, local_folder_name):
 		print("\n")
 		"""
 		# Using requests:
-		fcontent = requests.get(dl_url, allow_redirects=True)
+		fcontent = s.get(dl_url, allow_redirects=True)
 		received_size = len(fcontent.content)
 		if received_size == fsize:
 			open(out_path_file_name, 'wb').write(fcontent.content)
@@ -50,7 +50,7 @@ def Dl_file(fchild, local_folder_name):
 		else:
 			print("Error downloading %s: expected %d bytes, received %d bytes"%(out_path_file_name, fsize, received_size))
 
-def Dl_child(child, remote_folder_name, local_folder):
+def Dl_child(s, child, remote_folder_name, local_folder):
 	"""
 	>>> children[0].keys()
 	dict_keys(['cTag', 'createdBy', 'webUrl', 'createdDateTime', 'size', 'lastModifiedBy', 'parentReference', 'eTag', 'lastModifiedDateTime', 'name', 'reactions', 'fileSystemInfo', 'id', 'folder'])
@@ -59,7 +59,7 @@ def Dl_child(child, remote_folder_name, local_folder):
 	remote_folder_name = remote_folder_name + "/" + folder_name
 	str_child = ":"+remote_folder_name+":/children"
 	url = url_base+str_child
-	mychild = requests.get(url)
+	mychild = s.get(url)
 	dchild = mychild.json()
 	"""
 	To see a readale print of JSON
@@ -86,9 +86,9 @@ def Dl_child(child, remote_folder_name, local_folder):
 		For folders, there is a key "folder"
 		"""
 		if "folder" in fchild:
-			Dl_child(fchild, remote_folder_name, local_folder_name)
+			Dl_child(s, fchild, remote_folder_name, local_folder_name)
 		else:
-			Dl_file(fchild, local_folder_name)
+			Dl_file(s, fchild, local_folder_name)
 
 
 if __name__ == '__main__':
@@ -96,10 +96,13 @@ if __name__ == '__main__':
 				"Downloads all files from a publicly shared onedrive folder via api \
 				to the current folder, replicating the OneDrive folder structure.",
 				formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument("-url", action="store",  default = "https://1drv.ms/u/s!AocykQAvhWc9ax_8RRkxKELRnSs?e=Z443ZC",
+	parser.add_argument("-url", action="store",  default = "https://1drv.ms/u/s!AocykQAvhWc9ghh04i3ZpSy6jHsf?e=HutI4W",
 						dest="shared_url", 
 						help="OneDrive shared URL.")
-
+#CoversBR benchmark
+#https://1drv.ms/u/s!AocykQAvhWc9ghh04i3ZpSy6jHsf?e=HutI4W
+#Covers20k
+#https://1drv.ms/u/s!AocykQAvhWc9ax_8RRkxKELRnSs?e=Z443ZC
 	args_main = parser.parse_args()
 	shared_url = args_main.shared_url
 	shared_url_b64 = base64.urlsafe_b64encode(shared_url.encode('ascii')).decode('ascii')
@@ -107,19 +110,50 @@ if __name__ == '__main__':
 	#url_base = "https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb2N5a1FBdmhXYzlheF84UlJreEtFTFJuU3M_ZT1aNDQzWkM/root"
 	str_root= "?expand=children"
 	url = url_base+str_root
-	myroot = requests.get(url)
-	data = myroot.json()
+	headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+    'Content-Type': 'text/html',
+	'connection': 'keep-alive',
+	}
+	session = requests.Session()
+	myroot = session.get(url, headers = headers)
+	dt = data = myroot.json()
+	while "children@odata.nextLink" in dt:
+		r = session.get(dt['children@odata.nextLink'], headers = headers)
+		dt = r.json()
+		data['children'].extend(dt['value'])
 	"""
 	>>> data.keys()
 	dict_keys(['cTag', 'createdBy', 'webUrl', 'createdDateTime', 'size', 'lastModifiedBy', 'parentReference', 'children', '@odata.context', 'shared', 'eTag', 'children@odata.count', 'lastModifiedDateTime', 'name', 'children@odata.context', 'reactions', 'fileSystemInfo', 'id', 'folder'])
+	
+	dict_keys(['id', 'parentReference', 'children', 'lastModifiedBy', 'children@odata.count', 'shared', 'webUrl', 'lastModifiedDateTime', 'children@odata.nextLink', 'reactions', 'createdDateTime', 'children@odata.context', 'cTag', 'eTag', 'name', 'fileSystemInfo', 'size', '@odata.context', 'folder', 'createdBy'])
+	>>> data['children@odata.nextLink']
+	"https://api.onedrive.com/v1.0/shares('u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb2N5a1FBdmhXYzlnaGgwNGkzWnBTeTZqSHNmP2U9SHV0STRX')/items('3D67852F00913287!280')/children/?$skiptoken=MjAx"
+	
+	 myroot2 = s.get(data['children@odata.nextLink'], headers = headers, stream=True)
+	data2 = myroot2.json() 
+	>>> data2.keys()
+	dict_keys(['value', '@odata.context', '@odata.nextLink', '@odata.count'])
+	>>> data2['@odata.count']
+	9184
+	>>> data2['@odata.nextLink']
+	"https://api.onedrive.com/v1.0/shares('u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb2N5a1FBdmhXYzlnaGgwNGkzWnBTeTZqSHNmP2U9SHV0STRX')/items('3D67852F00913287!280')/children/?$skiptoken=NDAx"
+	>>> data['children'][0].keys()
+	dict_keys(['id', 'parentReference', 'cTag', 'lastModifiedBy', 'webUrl', 'lastModifiedDateTime', 'reactions', 'createdDateTime', 'createdBy', 'eTag', 'name', 'fileSystemInfo', 'size', 'folder'])
+	>>> data2['value'][0].keys()
+	dict_keys(['id', 'parentReference', 'cTag', 'lastModifiedBy', 'shared', 'webUrl', 'lastModifiedDateTime', 'reactions', 'createdDateTime', 'createdBy', 'eTag', 'name', 'fileSystemInfo', 'size', 'folder'])
+	data['children'].extend(data2['value'])
 	"""
 	child_count = data["folder"]["childCount"] 
 	children = data["children"] 
 	folder_name = ""
 	local_folder_name = "."
 	print("Downloading %d files/folders from %s"%(child_count, shared_url))
+	dwonloaded_folders_count = 0
 	for child in data["children"]:
 		if "folder" in child:
-			Dl_child(child, folder_name, local_folder_name)
+			Dl_child(session, child, folder_name, local_folder_name)
+			dwonloaded_folders_count += 1
 		else:
-			Dl_file(child, local_folder_name)
+			Dl_file(session, child, local_folder_name)
+	print("Finished downloading %d folders"%dwonloaded_folders_count)
